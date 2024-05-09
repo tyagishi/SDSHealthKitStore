a6bd9c75c6080675d5d5239e960972512e70cdc4
Tomoaki Yagishita <tyagishi@gmail.com>
2024-05-09 13:25:59 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SDSHealthKitStore",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SDSHealthKitStore",
+            targets: ["SDSHealthKitStore"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SDSHealthKitStore"),
+        .testTarget(
+            name: "SDSHealthKitStoreTests",
+            dependencies: ["SDSHealthKitStore"]),
+    ]
+)
